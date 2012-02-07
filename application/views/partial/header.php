f15a0dc92ac00083d644c735d1d965e68b26097e
Mathew Wong <mathew@ubuntu-11-04.(none)>
2012-02-07 17:58:55 +0800

    added favicon

--- application/views/partial/header.php
+++ application/views/partial/header.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<link rel="shortcut icon" href="<?=URL::to('/')?>img/favicon.png">
 <title>Welcome | 36Stories | Let your customers speak about you.</title>
 		<?=HTML::style('css/s36style.css')?> 
         <?=HTML::style('css/slider.css')?>
