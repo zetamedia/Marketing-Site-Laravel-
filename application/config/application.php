53c5729f04ae8ffe20f66f9b1514f5192a8ef02a
unknown <dcalpatura@.microsourcing.com>
2012-02-07 17:27:43 +0800

    Updating

--- application/config/application.php
+++ application/config/application.php
@@ -14,8 +14,9 @@ return array(
 	|
 	*/
 
-	//'url' => 'http://localhost/Marketing-Site-Laravel-/public',
-	'url' => 'http://beta.36stories.com',
+	'url' => 'http://localhost/Marketing-Site-Laravel-/public',
+	//'url' => 'http://beta.36stories.com',
+	
 	/*
 	|--------------------------------------------------------------------------
 	| Application Index
