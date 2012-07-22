7db47d514120e1529e76179699ae54bed55d3194
mathew <mathew@ubuntu.(none)>
2012-07-22 08:06:58 +0800

    fixing

--- application/routes/test.php
+++ application/routes/test.php
@@ -3,4 +3,8 @@ return array(
     'GET /test' => Array('do' => function() {       
        echo 'Hello World';
     }), 
+
+    'GET /test/(:any)' => function($name) {
+        print_r($name);
+    }
 );
